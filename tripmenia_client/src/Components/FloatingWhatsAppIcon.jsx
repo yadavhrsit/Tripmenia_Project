c3ebf796d7891f1f86fa6737c8a4f82fe0fe2da7
Harshit Yadav <inbox.harshityadav@gmail.com>
2024-06-05 00:35:49 +0530

    Create FloatingWhatsAppIcon.jsx

---
+++ tripmenia_client/src/Components/FloatingWhatsAppIcon.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
+
+const FloatingWhatsAppIcon = () => {
+  return (
+    <a
+      href="https://wa.me/9999999999"
+      className="hidden md:block fixed z-50 bottom-2 right-2 bg-green-500 text-white p-2 rounded-full shadow-lg hover:bg-green-600 transition"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FaWhatsapp size={34} />
+    </a>
+  );
+};
+
+export default FloatingWhatsAppIcon;
